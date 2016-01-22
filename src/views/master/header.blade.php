f79ee3907f544c1e2ef2aa4f352bc063e6f9fca4
Joao Alfredo <joao.alfredo@gmail.com>
2016-01-22 14:22:15 -0200

    remove unused files

--- src/views/master/header.blade.php
+++
@@ -1,8 +0,0 @@
-@section('header')
-<div class="header navbar navbar-inverse navbar-fixed-top">
-    <div class="header-inner">
-        @include('layout::shared.header')
-    </div>
-</div>
-<div class="clearfix"> </div>
-@show
