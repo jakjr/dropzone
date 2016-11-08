3af515189b9945aafdbbd967c0ce119c34f434ad
jakjr <joao.alfredo@gmail.com>
2016-11-08 17:16:07 -0200

    Intregrado com Laravel.

---
+++ src/Middleware/Dropzone.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Jakjr\Dropzone\Middleware;
+
+use Closure;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+use Symfony\Component\HttpFoundation\FileBag;
+
+class Dropzone
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+
+        if ($request->has('dropzone')) {
+
+            $dropzoneFiles = $request->get('dropzone');
+
+            foreach ($dropzoneFiles as $inputElementName=>$fileNames) {
+                $this->setBag($request, $inputElementName, $fileNames);
+            }
+
+            $request->request->remove('dropzone');
+        }
+
+        return $next($request);
+    }
+
+    private function setBag($request, $inputElementName, $fileNames)
+    {
+        $dropzone = app('dropzone');
+
+        foreach ($fileNames as $fileName) {
+
+            $path = $dropzone->baseDir . $fileName;
+
+            $uploadedFiles[] = new UploadedFile(
+                $path,
+                $fileName,
+                filetype($path),
+                filesize($path),
+                0,
+                true
+            );
+        }
+
+        $request->files->set($inputElementName, $uploadedFiles);
+    }
+}
